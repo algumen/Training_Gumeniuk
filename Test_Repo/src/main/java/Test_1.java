f2cbe286c1752245c367336d0a663a536c646bb4
algumen <algumen@gmail.com>
2015-06-14 12:12:17 +0300

    Из ПК

--- Test_Repo/src/main/java/Test_1.java
+++ Test_Repo/src/main/java/Test_1.java
@@ -1,5 +1,19 @@
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 /**
- * Created by Oleksii on 13.06.2015.
+ * Created by Oleksii on 31.05.2015.
  */
 public class Test_1 {
+
+    @Test
+    public void canAddTwoPlusTwo(){
+
+        int answer = 2+2;
+        Assert.assertEquals(answer, 4, "2+2");
+
+    }
+
 }
+
